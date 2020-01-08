fed694038ea4f0d2997173da297b1cc8f963dfbe
unknown <momin.bob@gmail.com>
2020-01-09 01:14:13 +0200

    first commit

---
+++ app/authors.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class authors extends Model
+{
+
+
+    public function books(){
+      return $this->hasMany('books');
+    }
+}
